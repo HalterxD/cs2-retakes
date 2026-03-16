d9ab1c7afdd184fd63ad7f7412e4b507a6feb23e
swastik-satpathy <swastiksatpathy@yahoo.com>
2026-03-16 06:54:34 +0530

    adding return in metamod

--- modules/gameinfo.sh
+++ modules/gameinfo.sh
@@ -7,7 +7,7 @@ GAMEINFO="/home/cs2server/serverfiles/game/csgo/gameinfo.gi"
 # Check if already configured
 if grep -q "csgo/addons/metamod" "$GAMEINFO"; then
     echo "Metamod already configured in gameinfo.gi"
-    exit 0
+    return
 fi
 
 # Insert right after Game_LowViolence csgo_lv line with proper indentation
